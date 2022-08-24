32d6a9d7eee535726ddcb89f11ac19abec738fdb
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-08-24 11:52:07 +0100

    Store the data inside of a function, change function and variable names

--- bank-tech-test/bank.js
+++ bank-tech-test/bank.js
@@ -1,51 +1,42 @@
 class Bank {
     constructor(balance) {
         this.balance = balance
+        this.statement = []
     }
 
     deposit(value, date){
         this.date = date
-        this.valueDep = value
+        this.depositValue = value
         this.balance += value
+        this.createObject()
     }
 
     withdraw(value, date){
         this.date = date
-        this.valueWith = value
+        this.withdrawValue = value
         this.balance -= value
+        this.createObject()
     }
     
     getTotal(){
         return this.balance
     }
 
-    createTable(){
+    createObject(){
         this.transaction = {
             'date': this.date, 
-            'credit': this.valueDep, 
-            'debit': this.valueWith, 
+            'credit': this.depositValue, 
+            'debit': this.withdrawValue, 
             'balance': this.balance
         }
-        return this.transaction
+        this.statement.push(this.transaction)
+        return this.statement
+    }
+
+    displayStatement(){
+        console.table(this.statement.reverse())
     }
 }
 
-// User makes their first transaction: deposit of 1000, the balance starts at zero.
-const firstTransaction = new Bank(0)
-firstTransaction.deposit(1000, '10/01/2023')
-const dataOne = firstTransaction.createTable()
-
-// User makes their second transaction: deposit of 2000.
-const secondTransaction = new Bank(firstTransaction.getTotal())
-secondTransaction.deposit(2000, '13/01/2023')
-const dataTwo = secondTransaction.createTable()
-
-// User makes their third transaction: withdraw of 500.
-const thirdTransaction = new Bank(secondTransaction.getTotal())
-thirdTransaction.withdraw(500, '14/01/2023')
-const dataThree = thirdTransaction.createTable()
-
-// Print the bank statement to the user on console
-console.table([dataOne, dataTwo, dataThree])
 
 module.exports = Bank;
