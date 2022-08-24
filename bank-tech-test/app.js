057882d2c328561ce140af7d4e0d631c761cff57
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-08-24 11:50:41 +0100

    Separate user interface in the new file

---
+++ bank-tech-test/app.js
@@ -0,0 +1,11 @@
+const Bank = require('./bank')
+
+const bank = new Bank(0);
+// User makes their first transaction: deposit of 1000, the balance starts at zero.
+bank.deposit(1000, '10/01/2023');
+// User makes their second transaction: deposit of 2000.
+bank.deposit(2000, '13/01/2023');
+// User makes their third transaction: withdraw of 500.
+bank.withdraw(500, '14/01/2023');
+// Print the bank statement to the user on console
+bank.displayStatement();
